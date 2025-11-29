5be30f7a60b1b5a989ee8bd6536358ab7d8b622f
alejozd <alejozd79@gmail.com>
2025-11-29 12:24:25 -0500

    se agregan campos nuevos

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,7 +22,7 @@ const User = sequelize.define(
   {
     tableName: "usuarios",
     timestamps: true,
-    createdAt: "fecha_registro",
+    createdAt: "created_at",
     updatedAt: "updated_at",
   }
 );
